ec92b97d79cca4b3bbcfc9ad792f91394371a408
Yosef Asouaqir <yosef.alsuoaqir@gmail.com>
2017-10-24 10:06:36 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,7 +1,11 @@
 var markers = [];
 
 var map;
-
+var CLIENT_ID;
+var CLIENT_SECRET;
+function startmap() {
+	ko.applyBindings(new initMap());
+}
 function initMap() {
   // Create a styles array to use with the map.
   var styles = [{
@@ -84,6 +88,11 @@ function initMap() {
     ]
   }];
 
+	var self = this;
+
+	this.searchTerm = ko.observable("");
+
+	this.locationList = ko.observableArray([]);
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
@@ -94,6 +103,7 @@ function initMap() {
     styles: styles,
     mapTypeControl: false
   });
+
   var tribeca = {
     lat: 24.751365,
     lng: 46.535520
@@ -140,14 +150,52 @@ function initMap() {
       }
     },
   ];
+	CLIENT_ID="SIAUYXUJLUUIQ5VPJJ0FGE1FTPOY1KZSXYQD4OY5D2LVR5M4";
+                   CLIENT_SECRET="ZS4TA0NYAD2NKUWAMH3GC4BFQ1XRMAQV5N04ZIRNRBCSXLT0";
+var Location = function(data) {
+	var self = this;
+	this.name = data.name;
+	this.lat = data.lat;
+	this.long = data.long;
+	this.URL = "";
+	this.street = "";
+	this.city = "";
+	this.phone = "";
 
+	this.visible = ko.observable(true);
+
+	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + CLIENT_ID + '&client_secret=' + clientSecret + '&v=20170801' + '&query=' + this.name;
+
+	$.getJSON(foursquareURL).done(function(data) {
+		var results = data.response.venues[0];
+		self.URL = results.url;
+		if (typeof self.URL === 'undefined'){
+			self.URL = "";
+		}
+		self.street = results.location.formattedAddress[0];
+     	self.city = results.location.formattedAddress[1];
+      	self.phone = results.contact.phone;
+      	if (typeof self.phone === 'undefined'){
+			self.phone = "";
+		} else {
+			self.phone = formatPhone(self.phone);
+		}
+	}).fail(function() {
+		alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
+	});
+
+	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content">' + self.phone + "</div></div>";
+	
         var largeInfowindow = new google.maps.InfoWindow();
         // Style the markers a bit. This will be our listing marker icon.
         var defaultIcon = makeMarkerIcon('0091ff');
         // Create a "highlighted location" marker color for when the user
         // mouses over the marker.
         var highlightedIcon = makeMarkerIcon('FFFF24');
-        var largeInfowindow = new google.maps.InfoWindow();
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < locations.length; i++) {
           // Get the position from the location array.
@@ -176,9 +224,28 @@ function initMap() {
             this.setIcon(defaultIcon);
           });
         }
+	initialLocations.forEach(function(locationItem){
+		self.locationList.push( new Location(locationItem));
+	});
+	this.filteredList = ko.computed( function() {
+		var filter = self.searchTerm().toLowerCase();
+		if (!filter) {
+			self.locationList().forEach(function(locationItem){
+				locationItem.visible(true);
+			});
+			return self.locationList();
+		} else {
+			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+				var string = locationItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				locationItem.visible(result);
+				return result;
+			});
+		}
+	}, self);
         document.getElementById('show-listings').addEventListener('click', showListings);
         document.getElementById('hide-listings').addEventListener('click', hideListings);
-      }
+      };
       // This function populates the infowindow when the marker is clicked. We'll only allow
       // one infowindow which will open at the marker that is clicked, and populate based
       // on that markers position.
