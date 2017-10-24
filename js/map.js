cbe223081da722372d3947935ab3712421f93cfe
Yosef Asouaqir <yosef.alsuoaqir@gmail.com>
2017-10-24 12:54:00 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,8 +1,117 @@
-var markers = [];
+var locations = [{
+      title: 'اسواق السلطان',
+      location: {
+        lat: 24.748644,
+        lng: 46.536133
+      }
+    },
+    {
+      title: 'حديقة الدرعية',
+      location: {
+        lat: 24.751547,
+        lng: 46.534889
+      }
+    },
+    {
+      title: 'مطعم بخاري',
+      location: {
+        lat: 24.750339,
+        lng: 46.538258
+      }
+    },
+    {
+      title: 'سد الدرعية',
+      location: {
+        lat: 24.747981,
+        lng: 46.544502
+      }
+    },
+    {
+      title: 'مكتبة الدرعية',
+      location: {
+        lat: 24.754704,
+        lng: 46.546347
+      }
+    },
+  ];
 
 var map;
+var markers = [];
+var clientID;
+var clientSecret;
 
-function initMap() {
+var Location = function(data) {
+	var self = this;
+	this.name = data.name;
+	this.lat = data.lat;
+	this.long = data.long;
+	this.URL = "";
+	this.street = "";
+	this.city = "";
+	this.phone = "";
+
+	this.visible = ko.observable(true);
+
+	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
+
+	$.getJSON(foursquareURL).done(function(data) {
+		var results = data.response.venues[0];
+		self.URL = results.url;
+		if (typeof self.URL === 'undefined'){
+			self.URL = "";
+		}
+		self.street = results.location.formattedAddress[0];
+     	self.city = results.location.formattedAddress[1];
+      	self.phone = results.contact.phone;
+	}).fail(function() {
+		alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
+	});
+	
+	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content">' + self.phone + "</div></div>";
+this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+
+	this.marker = new google.maps.Marker({
+			position: new google.maps.LatLng(data.lat, data.long),
+			map: map,
+			title: data.name
+	});
+
+	this.showMarker = ko.computed(function() {
+		if(this.visible() === true) {
+			this.marker.setMap(map);
+		} else {
+			this.marker.setMap(null);
+		}
+		return true;
+	}, this);
+
+	this.marker.addListener('click', function(){
+		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div></div>";
+
+        self.infoWindow.setContent(self.contentString);
+
+		self.infoWindow.open(map, this);
+
+		self.marker.setAnimation(google.maps.Animation.BOUNCE);
+      	setTimeout(function() {
+      		self.marker.setAnimation(null);
+     	}, 2100);
+	});
+
+	this.bounce = function(place) {
+		google.maps.event.trigger(self.marker, 'click');
+	};
+};
+
+function AppViewModel() {
   // Create a styles array to use with the map.
   var styles = [{
     featureType: 'water',
@@ -83,7 +192,11 @@ function initMap() {
       }
     ]
   }];
+var self = this;
 
+	this.searchTerm = ko.observable("");
+
+	this.locationList = ko.observableArray([]);
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
@@ -94,6 +207,37 @@ function initMap() {
     styles: styles,
     mapTypeControl: false
   });
+	clientID = "V443OTCAQPJLCRY4QWBFYN3ZK5FDKGJOYDHLMI3O342IRVNN";
+	clientSecret = "AK1JHLEG2D2KW14WF5HYVFNTUYFTBXYS4LDUUNRAHPR5URLB";
+
+	initialLocations.forEach(function(locationItem){
+		self.locationList.push( new Location(locationItem));
+	});
+
+	this.filteredList = ko.computed( function() {
+		var filter = self.searchTerm().toLowerCase();
+		if (!filter) {
+			self.locationList().forEach(function(locationItem){
+				locationItem.visible(true);
+			});
+			return self.locationList();
+		} else {
+			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+				var string = locationItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				locationItem.visible(result);
+				return result;
+			});
+		}
+	}, self);
+
+	
+	
+	
+	
+	
+	
+	
   var tribeca = {
     lat: 24.751365,
     lng: 46.535520
@@ -104,42 +248,7 @@ function initMap() {
     title: 'First Marker!'
   });
 
-  var locations = [{
-      title: 'اسواق السلطان',
-      location: {
-        lat: 24.748644,
-        lng: 46.536133
-      }
-    },
-    {
-      title: 'حديقة الدرعية',
-      location: {
-        lat: 24.751547,
-        lng: 46.534889
-      }
-    },
-    {
-      title: 'مطعم بخاري',
-      location: {
-        lat: 24.750339,
-        lng: 46.538258
-      }
-    },
-    {
-      title: 'سد الدرعية',
-      location: {
-        lat: 24.747981,
-        lng: 46.544502
-      }
-    },
-    {
-      title: 'مكتبة الدرعية',
-      location: {
-        lat: 24.754704,
-        lng: 46.546347
-      }
-    },
-  ];
+  
 
         var largeInfowindow = new google.maps.InfoWindow();
         // Style the markers a bit. This will be our listing marker icon.
@@ -223,6 +332,11 @@ function initMap() {
           new google.maps.Size(21,34));
         return markerImage;
   }
+
+function startApp() {
+	ko.applyBindings(new AppViewModel());
+}
+
 function errorHandling() {
-	alert("Error in Louding the map reconnect the internet");
+	alert("Google Maps has failed to load. Please check your internet connection and try again.");
 }
