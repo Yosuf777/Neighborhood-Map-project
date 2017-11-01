2e74714dc67fcd127d02a8e0bf632069aff1adda
Yosef Asouaqir <yosef.alsuoaqir@gmail.com>
2017-11-01 18:54:08 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,32 +2,44 @@ function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
           center: { lat: 24.751365,  lng: 46.535520},
-      zoom: 8,
+      zoom: 10,
         });
       }
+var favlocations = [{
+      title: 'mall',
+      location: {
+        lat: 24.748644,
+        lng: 46.536133
+      }
+    },
+    {
+      title: 'park',
+      location: {
+        lat: 24.751547,
+        lng: 46.534889
+      }
+    },
+    {
+      title: 'Resturant',
+      location: {
+        lat: 24.750339,
+        lng: 46.538258
+      }
+    },
+    {
+      title: 'River?',
+      location: {
+        lat: 24.747981,
+        lng: 46.544502
+      }
+    },
+    {
+      title: 'Library?',
+      location: {
+        lat: 24.754704,
+        lng: 46.546347
+      }
+    },
+  ];
 
-  
-function errorHandling() {
-	alert("Error in Louding the map reconnect the internet");
-}
-function AppViewModel() {
-    this.MALL = "MALL";
-    this.PARK = "PARK";
-	this.RESTERENT = "RESTERENT";
-	this.RIVER = "RIVER";
-	this.LIBERARY = "LIBERARY";
-}
 
-// Activates knockout.js
-//ko.applyBindings(new AppViewModel());
-
-ko.applyBindings({
-        locations: [
-            { MALL: 'MALL'},
-		{ PARK: 'PARK'},
-		  { RESTERENT: 'RESTERENT'},
-		  { RIVER: 'MALL'},
-		{ LIBERARY: 'LIBERARY'}
-		
-        ]
- });
