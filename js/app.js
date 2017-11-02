de75fa638015b2ba88ed2a48e594f8466a152029
Yosef Asouaqir <yosef.alsuoaqir@gmail.com>
2017-11-02 15:14:36 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -29,28 +29,44 @@ for(var i=1; i<tribeca.length; i++){
 }
  
 }
-function favLocations(name) {
-    var self = this;
-    self.name = name;
-}
 
 // Overall viewmodel for this screen, along with initial state
 function ViewModel() {
     var self = this;
 
-    // Editable data
-    self.locations = ko.observableArray([
-        new favLocations("mall"),
-        new favLocations("park"),
-        new favLocations("resturant"),
-     new favLocations("river"),
-        new favLocations("library")
-    ]);
+     self.name = name;
 
 self.filterText = ko.observable(""); // Text from search field
+ 
+
+    // Editable data
+self.locations = ko.observableArray([
+        { name: "park"},
+        { name: "Resturan"},
+        { name: "River"},
+        { name: "Library"},
+        { name: "mall"}
+    ]);
+    self.filteredlocations = ko.computed(function () {
+
+fText = self.filterText().replace(/\s+/g, ' ');
+
+var filteredlocations = ko.utils.arrayFilter(self.locations(), function(test) {
+            if(fText.length)
+                return ( test.name.toUpperCase().indexOf(fText.toUpperCase()) >= 0);
+            else
+                return 1;
+        });
+      
+        return filteredlocations;
+    }, self);
+    
 }
 
-ko.applyBindings(new ViewModel());
+$(document).ready( function(){
+    var vm = new ViewModel();
+    ko.applyBindings(vm);
+} );
 
 
 
