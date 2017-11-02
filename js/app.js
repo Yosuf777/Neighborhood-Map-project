da03144a7c7a3309292fc905030c73d294ff8250
Yosef Asouaqir <yosef.alsuoaqir@gmail.com>
2017-11-02 14:41:41 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,46 +2,33 @@ function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
           center: { lat: 24.751365,  lng: 46.535520},
-      zoom: 10,
+      zoom: 14,
         });
-      }
-var favlocations = [{
-      title: 'mall',
-      location: {
-        lat: 24.748644,
-        lng: 46.536133
-      }
-    },
-    {
-      title: 'park',
-      location: {
-        lat: 24.751547,
-        lng: 46.534889
-      }
-    },
-    {
-      title: 'Resturant',
-      location: {
-        lat: 24.750339,
-        lng: 46.538258
-      }
-    },
-    {
-      title: 'River?',
-      location: {
-        lat: 24.747981,
-        lng: 46.544502
-      }
-    },
-    {
-      title: 'Library?',
-      location: {
-        lat: 24.754704,
-        lng: 46.546347
-      }
-    },
-  ];
 
+
+
+var tribeca = new Array();
+
+tribeca =[
+{lat: 24.748644,  lng: 46.536133},
+{lat: 24.751547,  lng: 46.534889},
+{lat: 24.750339,  lng: 46.538258},
+{lat: 24.747981,  lng: 46.544502},
+{lat: 24.754704,  lng: 46.546347},
+{lat: 24.748644,  lng: 46.536133}
+];
+
+for(var i=1; i<tribeca.length; i++){
+        var marker = new google.maps.Marker({
+          position: tribeca[i] ,
+          map: map,
+          title: "PLACE NO: "+ i
+        });
+
+
+}
+ 
+}
 function favLocations(name) {
     var self = this;
     self.name = name;
@@ -59,6 +46,13 @@ function ViewModel() {
      new favLocations("river"),
         new favLocations("library")
     ]);
+
+self.filterText = ko.observable(""); // Text from search field
 }
 
 ko.applyBindings(new ViewModel());
+
+
+
+    
+  
